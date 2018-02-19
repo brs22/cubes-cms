b273aa3f1e66961e4d01d4a2fe70bc5d4759f38d
Boris <jelocnik.boris@gmail.com>
2018-02-19 16:31:39 +0100

    nakon casa

--- resources/lang/en/front_1.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-	
-	'main_title' => 'Website main title',
-	'contact_us_success' => 'Your message has been sent to us, we will contact you soon!',
-];
--- resources/lang/sr/front_1.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-	
-	'main_title' => 'Websajt glavni naslov',
-	
-	'contact_us_success' => 'Vasa poruka je poslata, uskoro cemo vas kontaktirati!',
-];
